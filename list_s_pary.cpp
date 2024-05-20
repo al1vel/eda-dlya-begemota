ac57faf879c937adcfe54f4cc041b3e008fd76d6
al1vel <m1233danil@bk.ru>
2024-05-20 10:54:56 +0300

    Сделал 8 кп

--- list_s_pary.cpp
+++ list_s_pary.cpp
@@ -2,60 +2,96 @@
 using namespace std;
 
 struct Node{
-    string val;
+    int val;
     Node* next{};
     Node* prev{};
+
+    Node() : val(0), next(nullptr), prev(nullptr) {}
+    Node(int v) : val(v), next(nullptr), prev(nullptr) {}
+    Node(Node *n) : val(n -> val), next(n -> next), prev(n -> prev) {}
 };
 
 struct List{
     Node* head;
     int length;
+
+    List() : head(nullptr), length(0) {}
+    List(int v, int len) {
+        head = new Node(v);
+        Node *newNode = new Node(v);
+        head -> next = newNode;
+        newNode -> prev = head;
+        length = len;
+        for (int i = 0; i < len - 2; ++i) {
+            newNode -> next = new Node(v);
+            newNode -> next -> prev = newNode;
+            newNode = newNode -> next;
+        }
+        newNode -> next = head -> next;
+    }
 };
 
-bool isEmptyList(List ls){ return ls.head == nullptr; }
+bool isEmptyList(List &ls){ return ls.head == nullptr; }
 
 void printNode(Node *ptn){
     if (ptn == nullptr) throw -1;
-    printf("%-13d %-10s %-13d %-13d\n", ptn, ptn -> val.c_str(), ptn -> next, ptn -> prev);
+    printf("%-13d %-10d %-13d %-13d\n", ptn, ptn -> val, ptn -> next, ptn -> prev);
 }
 
-void printList(Node *head){
+void printList(List &ls){
     printf("%-13s %-10s %-13s %-13s\n", "CUR ADDRESS", "VALUE", "NEXT ADDRESS", "PREV ADDRESS");
-    while (!isEmptyList(head)){
-        printNode(head);
-        head = head -> next;
+    Node *ptn = ls.head;
+    int len = ls.length;
+    for (int i = 0; i < len; ++i){
+        printNode(ptn);
+        ptn = ptn -> next;
     }
 }
 
-void deleteList(Node* & head){
-    Node *ptn = head;
-    while (!isEmptyList(head)){
-        ptn = head -> next;
-        delete head;
-        head = ptn;
+void deleteList(List &ls){
+    Node *ptn = ls.head;
+    while (!isEmptyList(ls)){
+        ptn = ptn -> next;
+        delete ls.head;
+        ls.head = ptn;
     }
 }
 
-void addNode(Node*&head, string val){
-    Node *newN = new Node;
-    if (isEmptyList(head)){
-        head = newN;
+void addNode(List &ls, int val){
+    Node *newN = new Node(val);
+    if (isEmptyList(ls)){
+        ls.head = newN;
         return;
     }
-    newN -> next = nullptr;
-    newN -> val = val;
-    Node *ptn = head;
-    while (ptn -> next != nullptr){
+    newN -> next = ls.head -> next;
+    Node *ptn = ls.head;
+    int len = ls.length;
+    for (int i = 0; i < len - 1; ++i) {
         ptn = ptn -> next;
     }
     ptn -> next = newN;
     newN -> prev = ptn;
+    ls.head -> next -> prev = newN;
+    ls.length += 1;
 }
+
+int list_len(List &ls) {
+    return ls.length;
+}
+
+void swap_sec_and_second_last(List &ls) {
+    Node* sec = ls.head->next;
+    Node* second_last = ls.head;
+    for (int i = 0; i < ls.length - 2; ++i) {
+        second_last = second_last->next;
+    }
+    swap(sec->val, second_last->val);
+}
+
 int main(){
-    Node *listH = nullptr;
-    listH = new Node;
-    listH -> val = "aha";
-    listH -> next = nullptr;
-    addNode(listH, "hui");
-    printList(listH);
+    List a(3, 4);
+    addNode(a, 10);
+    addNode(a, 20);
+    swap_sec_and_second_last(a);
+    printList(a);
 }
