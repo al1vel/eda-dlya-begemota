cd8b3d13645da15780525d8b2a4f275cc5a3c148
begemot <m1233danil@bk.ru>
2024-10-06 00:15:59 +0300

    task2

--- lab1_task2/task2.cpp
+++ lab1_task2/task2.cpp
@@ -1,4 +1,4 @@
-#include <locale>
+#include <ctype.h>
 #include <stdio.h>
 #include <math.h>
 
@@ -10,11 +10,14 @@ enum CODES {
     EPS_TOO_BIG
 };
 
-typedef float(*callback)(float);
+typedef double(*callback)(double);
 
-float fact(float n) {
-    if (n == 0) return 1.0;
-    else return n * fact(n - 1);
+double fact(double n) {
+    if (n == 0) {
+        return 1.0;
+    } else {
+        return n * fact(n - 1);
+    }
 }
 
 CODES ValidateEps(const char* argv) {
@@ -37,7 +40,7 @@ CODES ValidateEps(const char* argv) {
     if (dotCnt > 1) {
         return NOT_A_NUM;
     }
-    if (cnt > 10) {
+    if (cnt > 18) {
         return NUM_TOO_LARGE;
     }
     return VALID;
@@ -53,29 +56,100 @@ void ValidateCode(CODES code) {
     }
 }
 
-CODES ValidateEpsNum (float eps) {
+CODES ValidateEpsNum (double eps) {
+    //printf("%lf\n", eps);
     if (eps < 0) {
         return NEG_NUM;
-    } else if (eps > 1) {
+    } else if (eps > 0.2) {
         return EPS_TOO_BIG;
     }
     return VALID;
 }
 
-float funcForLim1(float n) {
-    float res = pow((1 + 1 / n), n);
+double funcForLim1 (double n) {
+    double res = pow((1 + 1 / n), n);
     return res;
 }
 
-float funcForSumm1(float n) {
-    float res = (1 / fact(n));
+double funcForSumm1 (double n) {
+    double res = (1 / fact(n));
     return res;
 }
 
-float limit(const float EPS, const callback func) {
-    float val;
-    float cur_val;
-    float x = 1;
+double funcForEq1 (double n) {
+    double res = log(n) - 1;
+    return res;
+}
+
+double funcForLim2 (double n) {
+    double res = pow((pow(2, n) * fact(n)),4) / (n * pow(fact(2 * n), 2));
+    return res;
+}
+
+double funcForSum2 (double n) {
+
+    int upPower = (int)n - 1;
+    //printf("%d\n", upPower);
+    double up = 1.0;
+
+    if (upPower % 2 == 1) {
+        up = -1.0;
+    }
+    double res = up / (2 * n - 1);
+    return res;
+    // double res = pow(-1, (n - 1)) / (2 * n - 1);
+    // return res;
+}
+
+double funcForEq2 (double n) {
+    double res = sin(n);
+    return res;
+}
+
+double funcForLim3 (double n) {
+    double res = n * (pow(2, 1 / n) - 1);
+    return res;
+}
+
+double funcForSum3 (double n) {
+    int upPower = (int)n - 1;
+    //printf("%d\n", upPower);
+    double up = 1.0;
+
+    if (upPower % 2 == 1) {
+        up = -1.0;
+    }
+    double res = up / n;
+    return res;
+}
+
+double funcForEq3 (double n) {
+    double res = pow(2.7182818284, n) - 2;
+    return res;
+}
+
+double funcForLim4 (double x) {
+    // static int flag = 0;
+    // double res;
+    // if (flag == 0) {
+    //     res = -0.5 - (-0.5 / 2 * -0.5) + 1;
+    //     flag = 1;
+    // } else {
+    //     res = n - (n * n / 2) + 1;
+    // }
+    // //printf("%lf, %lf\n", res, n);
+    // return res;
+    static double res;
+    if (x == 1) res = -0.5;
+    res = res - res * res / 2 + 1;
+    return res;
+}
+
+double limit(const double EPS, const callback func) {
+    double val;
+    double cur_val;
+    // double prev_val;
+    double x = 1;
     do {
         cur_val = func(x);
         x += 1;
@@ -85,18 +159,69 @@ float limit(const float EPS, const callback func) {
     return cur_val;
 }
 
-float summ (const float start, const float EPS, const callback func) {
-    float x = start;
-    float summ = 0;
-    float cur_summ = 0;
+// double limit4(const double EPS, const callback func) {
+//     double val;
+//     double cur_val;
+//     double x = 1;
+//     do {
+//         cur_val = func(x);
+//         x = func(x);
+//         val = func(x);
+//         printf("%.18f %.18f\n", val, cur_val);
+//     } while ((fabs(cur_val - val)) > EPS);
+//     return cur_val;
+// }
+
+double summ (const double start, const double EPS, const callback func, double multiplier) {
+    double x = start;
+    double summ = 0;
+
+    double val = func(x);
+    summ += func(x);
+    double prev = func(x);
+    x += 1;
 
     do {
-        cur_summ = summ;
-        summ += func(x);
+        val = func(x);
+        summ += val;
+        prev = val;
         x += 1;
-    } while ((fabs(cur_summ - summ)) > EPS);
+        //printf("%lf, %lf\n", val, prev);
+    } while (fabs(fabs(func(x) - fabs(prev))) > EPS);
 
-    return summ;
+    return summ * multiplier;
+}
+
+double equation(const double EPS, const callback func, double l, double r) {
+    double x = (l + r) / 2;
+    double prev = 100;
+
+    if ((func(r) - func(l)) > 0) {
+        //printf("test");
+        while (fabs(func(x) - prev) > EPS) {
+            //printf("%-5f %-5f %-5f %-5f\n", l, r, x, func(x));
+            if (func(x) > 0) {
+                r = x;
+            } else {
+                l = x;
+            }
+            prev = func(x);
+            x = (l + r) / 2;
+        }
+    } else {
+        while (fabs(func(x) - prev) > EPS) {
+            //printf("%-5f %-5f %-5f %.10f\n", l, r, x, func(x));
+            if (func(x) < 0) {
+                r = x;
+            } else {
+                l = x;
+            }
+            prev = func(x);
+            x = (l + r) / 2;
+        }
+    }
+
+    return l;
 }
 
 int main(const int argc, char* argv[]) {
@@ -111,16 +236,42 @@ int main(const int argc, char* argv[]) {
         return -1;
     }
 
-    const float eps = atof(argv[1]);
+    const double eps = atof(argv[1]);
     ret = ValidateEpsNum(eps);
     if (ret != VALID) {
         ValidateCode(ret);
+        return -1;
     }
-    printf("Count e\n");
-    printf("%6s, %6s, %6s", "");
-    float res_lim = limit(eps, &funcForLim1);
-    float res_sum = summ(0, eps, &funcForSumm1);
-    printf("%5f %5f", res_lim, res_sum);
 
-    return 0;
+    double res_lim = 0;
+    double res_sum = 0;
+    double res_eq = 0;
+
+    //HEADER
+    printf("%-8s | %-8s | %-8s\n", "LIMIT", "ROW", "EQUATION");
+    printf("==============================\n");
+
+    //COUNT E
+    res_lim = limit(eps, &funcForLim1);
+    res_sum = summ(0, eps, &funcForSumm1, 1);
+    res_eq = equation(eps, &funcForEq1, 1, 4);
+    printf("%-8f | %-8f | %-8f\n", res_lim, res_sum, res_eq);
+
+    // //COUNT PI
+    res_lim = limit(eps, &funcForLim2);
+    res_sum = summ(1, eps, &funcForSum2, 4);
+    res_eq = equation(eps, &funcForEq2, 2, 4);
+    printf("%-8f | %-8f | %-8f\n", res_lim, res_sum, res_eq);
+
+    // //COUNT ln2
+    res_lim = limit(eps, &funcForLim3);
+    res_sum = summ(1, eps, &funcForSum3, 1);
+    res_eq = equation(eps, &funcForEq3, 0, 2);
+    printf("%-8f | %-8f | %-8f\n", res_lim, res_sum, res_eq);
+
+    //COUNT sqrt2
+    res_lim = limit(eps, &funcForLim4);
+    printf("%-8f | %-8f | %-8f\n", res_lim, res_sum, res_eq);
+
+    printf("\n");return 0;
 }
