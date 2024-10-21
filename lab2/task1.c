d344c2f9b876f74f43f643fde9e147eb24973b13
begemot <m1233danil@bk.ru>
2024-10-21 20:28:23 +0300

    task 2.1 some changes

--- lab2/task1.c
+++ lab2/task1.c
@@ -1,6 +1,37 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+
+enum {
+    VALID,
+    NOT_A_NUM,
+    EMPTY_LINE
+} CODES;
+
+void ValidateCode(const int code) {
+    switch (code) {
+        case EMPTY_LINE: printf("empty line\n"); break;
+        case NOT_A_NUM: printf("not a number\n"); break;
+        default: printf("Unknown error code\n"); break;
+    }
+}
+
+int ValidateNum(const char* argv) {
+    int cnt = 0;
+    while (*argv != '\0') {
+        if (!isdigit(*argv)) {
+            return NOT_A_NUM;
+        } else {
+            argv++;
+            cnt++;
+        }
+    }
+    if (cnt == 0) {
+        return EMPTY_LINE;
+    }
+    return VALID;
+}
 
 int my_strcmp(const char *str1, const char *str2) {
     while (*str1 != '\0' && *str2 != '\0') {
@@ -116,11 +147,18 @@ char *newOrder(const char *str) {
     return result;
 }
 
+char *concat(char* str1, unsigned int seed, ...) {
+    va_list args;
+    va_start (args, seed);
+
+
+}
+
 int main(int argc, char *argv[]) {
     char* flags[] = { "-l", "-r", "-u", "-n", "-c" };
 
-    if (argc <= 1) {
-        printf("Flag must be inputted\n");
+    if (argc <= 2) {
+        printf("Wrong number of arguments\n");
         return -1;
     }
 
@@ -153,6 +191,18 @@ int main(int argc, char *argv[]) {
             char *res = newOrder(argv[2]);
             printf("NewOrder is %s\n", res);
             free(res);
+            break;
+        }
+        case 4: {
+            if (argc < 5) {
+                printf("Wrong number of arguments.\n");
+                return -1;
+            }
+            int ret_code = ValidateNum(argv[3]);
+            if (ret_code != VALID) {
+                ValidateCode(ret_code);
+                return -1;
+            }
             break;
         }
         default: {
