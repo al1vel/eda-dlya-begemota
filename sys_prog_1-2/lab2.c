f5e89b1e636ffdbbceb748922cfae6f1bce79117
begemot <m1233danil@bk.ru>
2025-04-08 01:06:00 +0300

    lab2 xor done, mask done (need check)

--- sys_prog_1-2/lab2.c
+++ sys_prog_1-2/lab2.c
@@ -1,7 +1,6 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <dirent.h>
 #include <pthread.h>
 #include <stdint.h>
 
@@ -14,6 +13,7 @@ int simplePow(int num, int exp) {
 }
 
 enum {
+    INVALID_NUMBER = -6,
     FILE_OPEN_FAILURE = -5,
     THREAD_CREATION_ERROR,
     MALLOC_FAIL,
@@ -30,6 +30,14 @@ struct xorThreadData {
     char *file_path;
     int bytesCnt;
     uint64_t xor_result;
+    int error;
+};
+
+struct maskThreadData {
+    char *file_path;
+    int mask;
+    int answer;
+    int error;
 };
 
 int contains(const char *str, const char* substr) {
@@ -77,40 +85,48 @@ int containsFlag(const char *arg) {
 
 void* threadXOR(void *arg) {
     struct xorThreadData *data = (struct xorThreadData*)arg;
-    printf("%s - %d\n", data->file_path, data->bytesCnt);
     FILE* file = fopen(data->file_path, "rb");
     if (file == NULL) {
-        data->xor_result = FILE_OPEN_FAILURE;
+        data->error = FILE_OPEN_FAILURE;
         return NULL;
     }
 
     uint64_t result = 0;
-    size_t size = data->bytesCnt == 0 ? 1 : data->bytesCnt;
+    int size = data->bytesCnt == 0 ? 1 : data->bytesCnt;
     uint8_t buffer[size];
 
-    size_t bytesRead = fread(buffer, 1, size, file);
+    int bytesRead = fread(buffer, 1, size, file);
     if (bytesRead == 0) {
         data->xor_result = 0;
         return NULL;
     }
 
     if (data->bytesCnt == 0) {
-        result = (buffer[0] >> 4) & 0xF0;
+        result = buffer[0] >> 4;
         result = result ^ (buffer[0] & 0x0F);
     } else {
         uint64_t block = 0;
         for (int i = 0; i < size; ++i) {
             block = block << 8;
-            block = buffer[i];
+            block |= buffer[i];
         }
         result = block;
     }
 
     while ((bytesRead = fread(buffer, 1, size, file)) > 0) {
-        for (size_t i = 0; i < bytesRead; i++) {
-            printf("%d ", buffer[i]);
+        if (data->bytesCnt == 0) {
+            result = result ^ (buffer[0] >> 4);
+            result = result ^ (buffer[0] & 0x0F);
+        } else {
+            uint64_t block = 0;
+            for (int j = 0; j < size; ++j) {
+                block = block << 8;
+                if (j < bytesRead) {
+                    block |= buffer[j];
+                }
+            }
+            result = result ^ block;
         }
-        printf("\n");
     }
     fclose(file);
     data->xor_result = result;
@@ -136,18 +152,95 @@ int funcXOR(char *arg[], int fileCnt, int N) {
             printf("Thread creation error");
             return THREAD_CREATION_ERROR;
         }
-        //printf("Thread %d created\n", i);
         thr_ids[i] = thr_id;
     }
 
     for (int i = 0; i < fileCnt; i++) {
         pthread_join(thr_ids[i], NULL);
-        if (data[i].xor_result == FILE_OPEN_FAILURE) {
+        if (data[i].error == FILE_OPEN_FAILURE) {
             printf("File open failure.\n");
             free(thr_ids);
             return FILE_OPEN_FAILURE;
         }
         printf("xor%d result of file %s: %ld\n", N, data[i].file_path, data[i].xor_result);
+    }
+
+    free(thr_ids);
+    return SUCCESS;
+}
+
+int getNumber(char *str) {
+    int result = 0, cnt = 0;
+    while (*str != '\n' && *str != '\0') {
+        if (cnt > 11) {
+            return INVALID_NUMBER;
+        }
+        if (!isdigit(*str)) {
+            return INVALID_NUMBER;
+        }
+        result = result * 10 + *str - '0';
+        str++;
+        cnt++;
+    }
+    return result;
+}
+
+void *threadMask(void *arg) {
+    struct maskThreadData *data = (struct maskThreadData*)arg;
+    FILE* file = fopen(data->file_path, "r");
+    if (file == NULL) {
+        data->error = FILE_OPEN_FAILURE;
+        return NULL;
+    }
+
+    int answer = 0;
+    char buf[11];
+    while (fgets(buf, 11, file)!= NULL) {
+        int num = getNumber(buf);
+        if (num == INVALID_NUMBER) {
+            data->error = INVALID_NUMBER;
+            printf("Invalid number.\n");
+            return NULL;
+        }
+        if ((num & data->mask) == data->mask) {
+            answer++;
+        }
+    }
+
+    fclose(file);
+    data->error = SUCCESS;
+    data->answer = answer;
+    return NULL;
+}
+
+int funcMASK(char *arg[], int fileCnt, int mask) {
+    pthread_t *thr_ids = (pthread_t*)malloc(fileCnt * sizeof(pthread_t));
+    if (thr_ids == NULL) {
+        printf("malloc failed\n");
+        return MALLOC_FAIL;
+    }
+
+    struct maskThreadData data[fileCnt];
+
+    for (int i = 0; i < fileCnt; i++) {
+        pthread_t thr_id = 0;
+        data[i].file_path = arg[i];
+        data[i].mask = mask;
+        if (pthread_create(&thr_id, NULL, &threadMask, &data[i]) != 0) {
+            printf("Thread creation error");
+            return THREAD_CREATION_ERROR;
+        }
+        thr_ids[i] = thr_id;
+    }
+
+    for (int i = 0; i < fileCnt; i++) {
+        pthread_join(thr_ids[i], NULL);
+        if (data[i].error != SUCCESS) {
+            printf("Failure.\n");
+            free(thr_ids);
+            return data[i].error;
+        }
+        printf("mask result of file %s: %d\n", data[i].file_path, data[i].answer);
     }
 
     free(thr_ids);
@@ -221,6 +314,11 @@ int main(const int argc, char *argv[]) {
             printf("Invalid argument to flag <mask>.\nArgument must be a hex number only.\n");
             return INVALID_FLAG;
         }
+
+        int ret = funcMASK(++argv, argc - 2, N);
+        if (ret != SUCCESS) {
+            return code;
+        }
     }
 
     else if (code == copy) {
@@ -293,7 +391,6 @@ int main(const int argc, char *argv[]) {
     if (pattern != NULL) {
         printf("pattern: (%s)\n", pattern);
     }
-    printf("N: %d\n", N);
 
 
     if (pattern != NULL) {
